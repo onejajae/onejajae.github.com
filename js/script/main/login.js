3ac5ac9a85efed3eeb692b479f5d8e443a434863
onejajae <onejajae@gmail.com>
2018-07-09 05:01:48 +0900

    add login cookie

--- js/script/main/login.js
+++ js/script/main/login.js
@@ -1,16 +1,10 @@
-var encryptedHTML = "/P2ou4GmzqUU38WOnNC1Ww==";
-
 function login(){
   var key = document.getElementById("passwordForm").value;
+
   Cookies.set("loginKey", key, { domain: 'github.onejajae.net' });
-  try{
-    var decryptedHTML = GibberishAES.aesDecrypt(encryptedHTML, key);
-    console.log(decryptedHTML);
-  } catch(e) {
-    console.log("error");
-  }
 }
 
 function logout(){
-
+  Cookies.remove("loginKey");
+  location.href = "/login"
 }
