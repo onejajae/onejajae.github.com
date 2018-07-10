17db61984855e7b355059e70da8f081c0a9e5694
onejajae <onejajae@gmail.com>
2018-07-10 16:23:20 +0900

    separate login page

--- js/script/main/login.js
+++ js/script/main/login.js
@@ -1,6 +1,6 @@
 function login(){
   var key = document.getElementById("passwordForm").value;
-  Cookies.set("loginKey", key, { domain: 'github.onejajae.net' });
+  Cookies.set("loginKey", key, {domain: location.hostname});
   location.href = "/"
 }
 
